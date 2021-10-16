e2a19b67fcc3d00c9ca9b5c4ce3432d34c84c732
Samantha Perez <sjenn257@gmail.com>
2021-10-16 09:06:22 -0500

    blank html pages added

---
+++ wine_data.py
@@ -0,0 +1,139 @@
+import pandas as pd
+
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+connect_string = "sqlite:///Resources/winedb2.db"
+
+engine = create_engine(connect_string)
+
+def top_wineries():
+
+    session = Session(engine)
+    sql = '''
+    SELECT winery, count(name) wines, avg(points) avg_points
+    FROM wine
+    group by winery having count(name)>2 and avg(points)>94
+    order by avg(points) DESC;
+    '''
+
+    df = pd.read_sql(sql, session.connection())
+
+    session.close()  
+    return df.to_dict(orient="records")     
+
+
+def wine_data():
+
+    session = Session(engine)
+    sql = '''
+    select name, variety, winery, region, state, description, points, price
+    from wine
+    order by state, region, winery, variety, name, points;
+    '''
+
+    df = pd.read_sql(sql, session.connection())
+
+    session.close()  
+    return df.to_dict(orient="records")    
+
+def winery_data():
+
+    session = Session(engine)
+    sql = '''
+    SELECT winery, count(name) wines, avg(points) avg_points, region, state
+    FROM wine
+    group by winery
+    order by state, region, winery;
+    '''
+
+    df = pd.read_sql(sql, session.connection())
+
+    session.close()  
+    return df.to_dict(orient="records")
+
+def total_wines():
+
+    session = Session(engine)
+    sql = '''
+    SELECT COUNT ( DISTINCT name ) AS "total_wines" 
+    FROM wine;
+    '''
+
+    df = pd.read_sql(sql, session.connection())
+
+    session.close()  
+    return df.to_dict(orient="records")
+
+def total_varieties():
+
+    session = Session(engine)
+    sql = '''
+    SELECT COUNT ( DISTINCT variety) AS "total_varieties" 
+    FROM wine;
+    '''
+
+    df = pd.read_sql(sql, session.connection())
+
+    session.close()  
+    return df.to_dict(orient="records")
+
+def total_wineries():
+
+    session = Session(engine)
+    sql = '''
+    SELECT COUNT ( DISTINCT winery) AS "total_wineries" 
+    FROM wine;
+    '''
+
+    df = pd.read_sql(sql, session.connection())
+
+    session.close()  
+    return df.to_dict(orient="records")
+
+def total_regions():
+
+    session = Session(engine)
+    sql = '''
+    SELECT COUNT ( DISTINCT region) AS "total_regions" 
+    FROM wine;
+    '''
+
+    df = pd.read_sql(sql, session.connection())
+
+    session.close()  
+    return df.to_dict(orient="records")
+
+# flavor = ''
+# variety = ''
+# region = ''
+# state = '' 
+# points_min=0
+# price_min=0
+# price_max=1000
+
+params=[]
+
+def filtered_wine(params): 
+    
+    session = Session(engine)
+    sql = '''
+    SELECT *
+    FROM wine;
+    '''
+
+    df = pd.read_sql(sql, session.connection())
+
+    session.close()  
+    return df.to_dict(orient="records")
+
+
+if __name__ == "__main__":
+    results = filtered_wine([''])
+    print(params)
+
+
+
+
+
+
