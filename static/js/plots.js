d680165e00ecb28e81be87ebd874f3102e9c4704
Samantha Perez <sjenn257@gmail.com>
2021-10-19 18:51:23 -0500

    added script tag to index

--- static/js/plots.js
+++ static/js/plots.js
@@ -15,108 +15,108 @@ d3.json(wineSelected).then((data) => {
 
 // ********************BUILD CHARTS*****************************
 
-// FUNCTION: create a function to clear out the divs in HTML and only show new data
-function resetData() {
+// // FUNCTION: create a function to clear out the divs in HTML and only show new data
+// function resetData() {
 
-    dropdownFlavor.html("");
-    dropdownRegion.html("");
-    barPoints.html("");
-    barPrices.html("");
-};
+//     dropdownFlavor.html("");
+//     dropdownRegion.html("");
+//     barPoints.html("");
+//     barPrices.html("");
+// };
 
 
-function buildCharts(flavor, region) {
+// function buildCharts(flavor, region) {
 
-    // reset data
-    resetData();
+//     // reset data
+//     resetData();
 
-    d3.json(wineSelected).then((data) => {
+//     d3.json(wineSelected).then((data) => {
 
-        let resultArray = data[0];
+//         let resultArray = data[0];
 
-        let name = resultArray.name;
-        let points = resultArray.points;
-        let prices = resultArray.price;
-        let description = resultArray.description;
-        let region = resultArray.region;
-        let state = resultArray.state;
-        let variety = resultArray.variety;
-        let winery = resultArray.winery;
+//         let name = resultArray.name;
+//         let points = resultArray.points;
+//         let prices = resultArray.price;
+//         let description = resultArray.description;
+//         let region = resultArray.region;
+//         let state = resultArray.state;
+//         let variety = resultArray.variety;
+//         let winery = resultArray.winery;
 
-        let tracePoints = {
+//         let tracePoints = {
 
-            x: points,
-            y: name,
-            text: description,
-            type: 'bar',
-            orientation: 'h'
-        };
+//             x: points,
+//             y: name,
+//             text: description,
+//             type: 'bar',
+//             orientation: 'h'
+//         };
 
-        let tracePoints = [tracePoints];
+//         let tracePoints = [tracePoints];
         
-        let trace1layout = {
-            margin: { t: 30, l: 30 } 
-        };
+//         let trace1layout = {
+//             margin: { t: 30, l: 30 } 
+//         };
         
-        Plotly.newPlot("barPoints", tracePoints, trace1Layout);
+//         Plotly.newPlot("barPoints", tracePoints, trace1Layout);
 
-        let trace2 = {
+//         let trace2 = {
 
-            x: prices,
-            y: name,
-            text: description,
-            type: 'bar',
-            orientation: 'h'
-        };
+//             x: prices,
+//             y: name,
+//             text: description,
+//             type: 'bar',
+//             orientation: 'h'
+//         };
 
-        let tracePrices = [trace2];
+//         let tracePrices = [trace2];
         
-        let trace2layout = {
-            margin: { t: 30, l: 30 } 
-        };
+//         let trace2layout = {
+//             margin: { t: 30, l: 30 } 
+//         };
         
-        Plotly.newPlot("barPrices", tracePricesS, trace2Layout);
+//         Plotly.newPlot("barPrices", tracePricesS, trace2Layout);
 
-    });
+//     });
 
-};
+// };
 
-function init() {
+// function init() {
 
-    // read in data from flavors
-    d3.json('/api/v1.0/flavors').then((data => {
+//     // read in data from flavors
+//     d3.json('/api/v1.0/flavors').then((data => {
 
-        // for loop to fill in the drop down lists
-        data.flavor.forEach((name => {
-            let option = dropdownFlavor.append("option");
-            option.text(name);
-        }));
-    }));
+//         // for loop to fill in the drop down lists
+//         data.flavor.forEach((name => {
+//             let option = dropdownFlavor.append("option");
+//             option.text(name);
+//         }));
+//     }));
 
-    d3.json('/api/v1.0/regions').then((data => {
+//     d3.json('/api/v1.0/regions').then((data => {
 
-        // for loop to fill in the drop down lists
-        data.region.forEach((name => {
-            let option = dropdownRegion.append("option");
-            option.text(name);
-        }));
+//         // for loop to fill in the drop down lists
+//         data.region.forEach((name => {
+//             let option = dropdownRegion.append("option");
+//             option.text(name);
+//         }));
 
-    }));
+//     }));
     
-    // set the default to all
-    let defaultFlavor = dropdownFlavor.property("all");
-    let defaultRegion = dropdownRegion.property("all");
+//     // set the default to all
+//     let defaultFlavor = dropdownFlavor.property("all");
+//     let defaultRegion = dropdownRegion.property("all");
 
-    // plot charts with defaults
-    buildCharts(defaultFlavor, defaultRegion);
+//     // plot charts with defaults
+//     buildCharts(defaultFlavor, defaultRegion);
 
-};
+// };
 
-function optionChanged(newFlavor, newRegion) {
+// function optionChanged(newFlavor, newRegion) {
 
-    buildCharts(newFlavor, newRegion);
+//     buildCharts(newFlavor, newRegion);
 
-};
+// };
 
-// Initialize the dashboard
-init();
+// // Initialize the dashboard
+// init();
