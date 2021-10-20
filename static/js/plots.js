35d3dea241cc8f4aaccee64a494a6338709071e9
Samantha Perez <sjenn257@gmail.com>
2021-10-19 22:59:32 -0500

    updated plotly

--- static/js/plots.js
+++ static/js/plots.js
@@ -3,18 +3,20 @@
 // global variables used for array lists
 var flavorsIn = [];
 var regionsIn = [];
+var winesIn = [];
 
 // variable for routes
 var url_flavors = '/api/v1.0/flavors';
 var url_regions = '/api/v1.0/regions';
+var url_wines = '/api/v1.0/filteredwine/all/all';
 
-var urls = [url_flavors, url_regions];
+var urls = [url_flavors, url_regions, url_wines];
 
 var promises = [];
 
 urls.forEach(function(url){promises.push(d3.json(url))});
 
-// console.log(promises);
+console.log(promises);
 Promise.all(promises).then(data => init(data));
 
 
@@ -24,87 +26,11 @@ let dropdownRegion = d3.select("#selRegion");
 let barPoints = d3.select("#barPoints");
 let barPrices = d3.select("#barPrices");
 
-var wineSelected = `/api/v1.0/filteredwine/all/all`; // keeping it all for now since filter isn't working
-
-// ********************TESTING THE DATA FILE********************
-
-// d3.json(wineSelected).then((data) => {
-//     console.log('data', data)}); // this works!
-
-// ********************BUILD CHARTS*****************************
-
-
-// function buildCharts(flavor, region) {
-
-
-//     d3.json(wineSelected).then((data) => {
-
-//         let resultArray = data[0];
-
-//         let name = resultArray.name;
-//         let points = resultArray.points;
-//         let prices = resultArray.price;
-//         let description = resultArray.description;
-//         let region = resultArray.region;
-//         let state = resultArray.state;
-//         let variety = resultArray.variety;
-//         let winery = resultArray.winery;
-
-//         let tracePoints = {
-
-//             x: points,
-//             y: name,
-//             text: description,
-//             type: 'bar',
-//             orientation: 'h'
-//         };
-
-//         let tracePoints = [tracePoints];
-        
-//         let trace1layout = {
-//             margin: { t: 30, l: 30 } 
-//         };
-        
-//         Plotly.newPlot("barPoints", tracePoints, trace1Layout);
-
-//         let trace2 = {
-
-//             x: prices,
-//             y: name,
-//             text: description,
-//             type: 'bar',
-//             orientation: 'h'
-//         };
-
-//         let tracePrices = [trace2];
-        
-//         let trace2layout = {
-//             margin: { t: 30, l: 30 } 
-//         };
-        
-//         Plotly.newPlot("barPrices", tracePricesS, trace2Layout);
-
-//     });
-
-// };
-
-    
 function init(promisedata) {
-
-    // read in data from flavors
-    // d3.json('/api/v1.0/flavors').then((data => {
-    //     console.log(data);
-    //     // for loop to fill in the drop down lists
-    //     data.flavor.forEach((name => {
-    //         let option = dropdownFlavor.append("option");
-    //         // console.log(name)
-    //         option.text(name);
-    //     }));
-    // }));
 
     flavorsIn = promisedata[0];
     regionsIn = promisedata[1];
-
+    winesIn = promisedata[2];
     console.log('flavors', flavorsIn);
     console.log('regions', regionsIn);
 
@@ -118,30 +44,107 @@ function init(promisedata) {
         option.text(region);
     }));
 
-    // d3.json('/api/v1.0/regions').then((data => {
-
-    //     // for loop to fill in the drop down lists
-    //     data.region.forEach((name => {
-    //         let option = dropdownRegion.append("option");
-    //         option.text(name);
-    //     }));
-
-    // }));
     
-    // set the default to all
-    let defaultFlavor = dropdownFlavor.property("all");
-    let defaultRegion = dropdownRegion.property("all");
 
-    // plot charts with defaults
-    buildCharts(defaultFlavor, defaultRegion);
+    // plot charts
+    buildChartPoints(winesIn);
+    buildChartPrices(winesIn);
+
 
 };
 
-function flavorChanged(newFlavor, newRegion) {
+function flavorChanged(newFlavor) {
+    // console.log(`change ${newFlavor}`)
 
-    buildCharts(newFlavor, newRegion);
+    let changeRegion = document.getElementById("selRegion").value;
+    urls[2]=(`/api/v1.0/filteredwine/${newFlavor}/${changeRegion}`)
+
+    // console.log('region value', changeRegion);
+
+    // console.log(urls[2]);
+    // console.log(winesIn);
+    // window.open(urls[2]);
+
+    d3.json(urls[2]).then(data => buildChartPoints(data));
+    d3.json(urls[2]).then(data => buildChartPrices(data));
+    // buildCharts(newFlavor, newRegion);
+
+};
+
+function regionSelected(newRegion) {
+    // console.log(`change ${newRegion}`)
+    let changeFlavor = document.getElementById("selFlavor").value;
+    urls[2]=(`/api/v1.0/filteredwine/${changeFlavor}/${newRegion}`)
+
+    // console.log(urls[2]);
+    // console.log(winesIn);
+    // window.open(urls[2]);
+
+    d3.json(urls[2]).then(data => buildChartPoints(data));
+    d3.json(urls[2]).then(data => buildChartPrices(data));
+    // buildCharts(newFlavor, newRegion);
 
 };
 
 // Initialize the dashboard
 init();
+
+
+// ********************BUILD CHARTS*****************************
+
+
+function buildChartPoints(wineData) {
+
+var wine_name = [];
+var points = [];
+
+    wineData.forEach((wine => {
+        wine_name.push(wine.name);
+        points.push(wine.points);
+    }));
+
+    let trace1 = {
+
+        x: points.reverse(),
+        y: wine_name,
+        type: 'bar',
+        orientation: 'h'
+    };
+
+    let tracePoints = [trace1];
+    
+    let trace1Layout = {
+        margin: { t: 30, l: 30 } 
+    };
+    
+    Plotly.newPlot("barPoints", tracePoints, trace1Layout);
+
+};
+
+function buildChartPrices(wineData) {
+
+    var wine_name = [];
+    var price = [];
+    
+        wineData.forEach((wine => {
+            wine_name.push(wine.name);
+            price.push(wine.price);
+        }));
+    
+        let trace2 = {
+    
+            x: price,
+            y: wine_name,
+            type: 'bar',
+            orientation: 'h'
+        };
+    
+        let tracePrices = [trace2];
+        
+        let trace2Layout = {
+            margin: { t: 30, l: 30 } 
+        };
+        
+        Plotly.newPlot("barPrices", tracePrices, trace2Layout);
+    
+    };
